54256fdc53c86a7e56966b7281895621fe41a30d
minhtuyen <minhtuyen300920011@gmail.com>
2020-10-14 20:57:43 +0700

    1

--- src/service/TranslateService.java
+++ src/service/TranslateService.java
@@ -2,31 +2,24 @@ package service;
 
 import controller.Controller;
 import helper.ResultTranslator;
-import helper.ShowText;
 import helper.Translator;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
 import model.Word;
-import view.SearchingPane;
 
 public class TranslateService extends Service<String> {
     private String text;
     private String res;
     public TranslateService(String text) {
         this.text = text;
-        setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent workerStateEvent) {
-                //show explain
-                if (text.equals("!@#firstcallapi")) {
-                    return;
-                }
-                if (!res.equals("")) {
-                    ResultTranslator resultTranslator = (ResultTranslator) new Controller();
-                    resultTranslator.Result(new Word(text,res));
-                }
+        setOnSucceeded(workerStateEvent -> {
+            //show explain
+            if (text.equals("!@#firstcallapi")) {
+                return;
+            }
+            if (!res.equals("")) {
+                ResultTranslator resultTranslator = (ResultTranslator) new Controller();
+                resultTranslator.Result(new Word(text,res));
             }
         });
     }
@@ -34,7 +27,7 @@ public class TranslateService extends Service<String> {
     protected Task<String> createTask() {
         return new Task<String>() {
             @Override
-            protected String call() throws Exception {
+            protected String call() {
                 res = Translator.translate(text);
                 return res ;
             }
